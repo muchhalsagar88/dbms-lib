f8c6bd46c399942623a3291959012f5e30b9bc21
muchhalsagar88 <muchhalsagar88@gmail.com>
2015-10-19 16:41:07 -0400

    Initial CLI commit

---
+++ src/main/java/edu/dbms/library/cli/route/RouteConstant.java
@@ -0,0 +1,23 @@
+package edu.dbms.library.cli.route;
+
+public class RouteConstant {
+
+	public static final String LOGIN = "/login";
+	
+	public static final String PATRON_BASE = "/patron";
+	
+	public static final String PATRON_PROFILE = "/patron/profile";
+	
+	public static final String PATRON_RESOURCES = "/patron/resources";
+	
+	public static final String PATRON_CHECKED_OUT = "/patron/checkedout";
+	
+	public static final String PATRON_RES_REQUEST = "/patron/resrequests";
+	
+	public static final String PATRON_NOTIFICATIONS = "/patron/notifications";
+	
+	public static final String PATRON_BALANCE = "/patron/balance";
+	
+	public static final String LOGOUT = "/logout";
+	
+}
---
+++ src/main/java/edu/dbms/library/cli/screen/PatronScreen.java
@@ -0,0 +1,78 @@
+package edu.dbms.library.cli.screen;
+
+import dnl.utils.text.table.TextTable;
+import edu.dbms.library.cli.Constant;
+import edu.dbms.library.cli.route.Route;
+import edu.dbms.library.cli.route.RouteConstant;
+import edu.dbms.library.cli.route.RouteController;
+
+public class PatronScreen extends BaseScreen {
+
+	public PatronScreen() {
+		super();
+		options.put(1, new Route(RouteConstant.PATRON_PROFILE));
+		options.put(2, new Route(RouteConstant.PATRON_RESOURCES));
+		options.put(3, new Route(RouteConstant.PATRON_CHECKED_OUT));
+		options.put(4, new Route(RouteConstant.PATRON_RES_REQUEST));
+		options.put(5, new Route(RouteConstant.PATRON_NOTIFICATIONS));
+		options.put(6, new Route(RouteConstant.PATRON_BALANCE));
+		options.put(7, new Route(RouteConstant.LOGOUT));
+	}
+	
+	@Override
+	public void execute() {
+		displayOptions();
+		readInputLabel();
+		Object o = readInput();
+		while(!(o instanceof Integer)) {
+			System.out.println("Incorrect input.");
+			readInputLabel();
+			o = readInput();
+		}
+		
+		Class t = RouteController.getInstance().getScreen(options.get((Integer)o).getRouteKey());
+		BaseScreen nextScreen;
+		try {
+			nextScreen = (BaseScreen)t.newInstance();
+			nextScreen.execute();
+		} catch (InstantiationException | IllegalAccessException e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
+
+	public void readInputLabel() {
+		System.out.print("Enter your choice: ");
+	}
+	
+	public Object readInput() {
+		int option = 0;
+		//while(inputScanner.hasNextInt()){
+			option = inputScanner.nextInt();
+		//}
+		return option;
+	}
+	
+	public void displayOptions() {
+		
+		String[] title = {""};
+		String[][] options = { 
+							{Constant.OPTION_PROFILE},
+							{Constant.OPTION_RESOURCES},
+							{Constant.OPTION_CHKDOUT_RES},
+							{Constant.OPTION_RES_REQUEST},
+							{Constant.OPTION_NOTIFICATIONS},
+							{Constant.OPTION_BALANCE},
+							{Constant.OPTION_LOGOUT}
+							};
+		TextTable tt = new TextTable(title, options);
+		tt.setAddRowNumbering(true);
+		//tt.setSort(0);
+		tt.printTable();
+	}
+	
+	public static void main(String []args) {
+		new PatronScreen().execute();
+	}
+}
