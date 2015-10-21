3b68f65a8ad85cf329d50129a1a70344f77d9df8
muchhalsagar88 <muchhalsagar88@gmail.com>
2015-10-21 02:57:39 -0400

    Added Login functionality to CLI
    Created the Login Screen accpeting username/password
    Checked for valid uname/pwd combination for a valid user
    Set session object for a valid user

---
+++ src/main/java/edu/dbms/library/db/manager/DBManager.java
@@ -0,0 +1,6 @@
+package edu.dbms.library.db.manager;
+
+public class DBManager {
+
+	protected static String DEFAULT_PERSISTENCE_UNIT_NAME = "main";
+}
---
+++ src/main/java/edu/dbms/library/db/manager/LoginManager.java
@@ -0,0 +1,41 @@
+package edu.dbms.library.db.manager;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import edu.dbms.library.entity.LoginDetails;
+import edu.dbms.library.to.LoginTO;
+
+public class LoginManager extends DBManager {
+
+	public static LoginTO checkCredentials(String username, String password) {
+		
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory(
+				DEFAULT_PERSISTENCE_UNIT_NAME);
+		EntityManager entitymanager = emfactory.createEntityManager( );
+		
+		LoginDetails login = (LoginDetails) entitymanager.find(LoginDetails.class, username);
+		if(login==null || !login.getPassword().equals(password)) 
+			return null;
+		
+		Query q = entitymanager.createQuery("SELECT s FROM LoginDetails l, Student s WHERE "
+				+ "l.patron.id = s.id "
+				+ "AND l.patron.id = :id");
+		q.setParameter("id", login.getPatron().getId());
+		
+		Object student = q.getSingleResult();
+		
+		LoginTO loginTo = new LoginTO(login.getPatron().getId());
+		if(student != null)
+			loginTo.setStudent(true);
+		
+		entitymanager.close();
+		emfactory.close();
+		
+		return loginTo;
+	}
+	
+}
+
