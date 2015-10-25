607ab7173772405900e2e909dfa8cb26fd2d3f01
raviflipsyde <ravi.flipsyde@gmail.com>
2015-10-25 16:55:25 -0400

    CLI for books, journal, cp

--- src/main/java/edu/dbms/library/entity/resource/Journal.java
+++ src/main/java/edu/dbms/library/entity/resource/Journal.java
@@ -1,10 +1,15 @@
 package edu.dbms.library.entity.resource;
 
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
+import edu.dbms.library.entity.Author;
 import edu.dbms.library.entity.Library;
 
 @Entity
@@ -36,5 +41,21 @@ public class Journal extends Publication {
 
 	public void setIssnNumber(String issnNUmber) {
 		this.issnNumber = issnNUmber;
+	}
+	
+	public Object[] toObjectArray() {
+		List<Object> objects = new LinkedList<Object>();
+		objects.add(this.issnNumber);
+		objects.add(this.getTitle());
+		Collection<Author> authList = getAuthors();
+		
+		StringBuffer authrs = new StringBuffer(""); 
+		for(Author a: authList){
+			authrs.append(a.getName()+",");
+		}
+		objects.add(authrs.toString());
+		objects.add(this.getPublicationYear());
+		
+		return objects.toArray();
 	}
 }
