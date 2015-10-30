b522ab010343e0b9859169ac86b361968b14fbd7
muchhalsagar88 <muchhalsagar88@gmail.com>
2015-10-30 11:26:49 -0400

    Merge issues Login Screen

--- src/main/java/edu/dbms/library/cli/screen/LoginScreen.java
+++ src/main/java/edu/dbms/library/cli/screen/LoginScreen.java
@@ -20,7 +20,6 @@ public class LoginScreen extends BaseScreen {
 		boolean validUser = false;
 		do {
 			String username = readInput("Enter Username");
-<<<<<<< HEAD
 			String password = null;
 			try{
 				password = new String(System.console().readPassword("Enter Password: "));
@@ -29,11 +28,6 @@ public class LoginScreen extends BaseScreen {
 				password = readInput("Enter Password");
 			}
 			password = LoginManager.getOraHash(password, bucketSize, salt);
-//			String username = "arpit";
-//			String password = "tyagi";
-=======
-			String password = readInput("Enter Password");
->>>>>>> 9070428c6021ab544c00bac22e74041fb8f6b4df
 			validUser = validateCredentials(username, password);
 			if(!validUser)
 				System.out.println("Username password combination is incorrect");
